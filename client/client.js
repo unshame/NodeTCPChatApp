578d3b58319ed831821344917bb6df8c2e5afead
UnShame <mrunshame@gmail.com>
2018-05-21 02:03:49 +0300

    Refactor; fix prompt position after print

--- client/client.js
+++ client/client.js
@@ -2,34 +2,29 @@ const args = require('minimist')(process.argv);
 const net = require('net');
 const readline = require('readline');
 
-const prompt = 'You> ';
-
-let reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: prompt
-});
-
-reader.on('line', line => {
-    let data = {
-        text: line
-    };
-    socket.write(JSON.stringify(data));
-    reader.prompt();
-});
-
 let port = args.p || args.port || 5000;
 let ip = args.ip || '127.0.0.1';
 
-
+// Открываем сокет для связи с сервером
 let socket = new net.Socket();
+
+// Считыватель строк из консоли
+let reader;
+
+// Пытаемся подключиться к серверу
 try {
     socket.connect(port, ip, () => {
         print(`Connected to ${ip}:${port}\n`);
+
+        // Отправляем имя клиента после подключения
         let data = {
             name: args.n || args.name || ''
         };
+
         socket.write(JSON.stringify(data));
+
+        // начинаем считывание из консоли
+        reader = createReader('You> ');
         reader.prompt();
     });
 }
@@ -38,24 +33,65 @@ catch(err) {
     process.exit(1);
 }
 
+// Обработчик данных от сервера
 socket.on('data', data => {
     print(data.toString());
 });
 
+// Обработчик закрытия соединения
 socket.on('close', () => {
     print('Connection closed\n');
     process.exit(0);
 });
 
+// Обработчик обрыва соединения
 socket.on('error', err => {
     print(`${err}\n`);
     process.exit(1);
 });
 
+// Создает и возвращает считыватель строк
+function createReader(prompt) {
 
+    // Считыватель
+    let reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: prompt
+    });
+
+    // Обработчик введенных строк
+    reader.on('line', line => {
+
+        // Мы будем передавать данные в виде преобразованного в строку объекта,
+        // чтобы дать возможность отправлять пустые сообщения
+        let data = {
+            text: line
+        };
+
+        // Преобразовываем текст в JSON строку и передаем серверу
+        socket.write(JSON.stringify(data));
+
+        // Читаем дальше
+        reader.prompt();
+    });
+
+    return reader;
+}
+
+// Выводит строку в консоль, сохраняя позицию промпта
 function print(str) {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
+
+    // Очищаем строку и переводим курсор в нулевую позицию,
+    // чтобы промпт не выводился со строкой
+    readline.clearLine();
+    readline.cursorTo(process.stdout, 0);
+
+    // Выводим строку
     process.stdout.write(str);
-    process.stdout.write(prompt);
+
+    // Возвращаем промпт на место
+    if(reader) {
+        reader.prompt(true);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -4,104 +4,117 @@ const net = require('net');
 let port = args.p || args.port || 5000;
 let ip = args.ip || '127.0.0.1';
 
+// Сокеты клиентов
 let sockets = [];
+
+// Использованные имена клиентов
 let names = {
     "you": {}
 };
+
+// Индекс пользователей без имени
 let userIndex = 1;
+
+// Серверные команды
 let commands = {
-    info: (socket) => {
-        let info = 'Currently in chat:\n';
-        sockets.forEach(otherSocket => {
-            let sinceActiveFormatted = formatTime(Date.now() - otherSocket.lastActivity);
-
-            if(otherSocket.name) {
-                info += `  ${otherSocket.name} (${otherSocket.info}) `;
-            }
-            else {
-                info += `  ${otherSocket.info} `;
-            }
-
-            info += `sent ${otherSocket.messagesNum} messages`;
-            if(otherSocket.messagesNum > 0) {
-                info += ` (last ${ sinceActiveFormatted } ago)`;
-            }
-            info += '\n';
-        });
-        socket.write(info);
-    },
-    help: (socket) => {
-        let help = 'Commands:\n';
-        Object.keys(commands).forEach(command => {
-            help += `  /${command}\n`;
-        });
-        socket.write(help);
-    }
+    info: commandInfo,
+    help: commandHelp
 };
 
+// Создаем TCP сервер
 let server = net.createServer(socket => addSocket(socket));
 
-server.listen(port, ip, () => {
-    console.log(`Listening on ${ip}:${port}`);
-});
+// Ожидаем пакеты
+server.listen(port, ip, () => console.log(`Listening on ${ip}:${port}`));
 
+// Добавляет сокет в массив и добавляет обработчики событий на сокет
 function addSocket(socket) {
+
+    if (sockets.includes(socket)) {
+        return;
+    }
+
+    // Статистика сокета
     socket.lastActivity = Date.now();
     socket.messagesNum = 0;
 
+    // Добавляем в массив сокетов
     sockets.push(socket);
 
+    // Обработчик данных от сокета
     socket.on('data', data => handleData(socket, data));
 
+    // Обработчик отключения сокета
     socket.on('end', () => removeSocket(socket));
 
+    // Обработчик разрыва соединения с сокетом
     socket.on('error', (err) => {
         console.error(err);
         removeSocket(socket);
     });
 }
 
+// Удаляет сокет из массива
 function removeSocket(socket) {
 
     if(!sockets.includes(socket)) {
         return;
     }
 
+    // Удаляем сокет из массива
     sockets.splice(sockets.indexOf(socket), 1);
+
+    // Удаляем имя сокета из используемых
     if (socket.name) {
         names[socket.name.toLowerCase()] = undefined;
     }
+
+    // Сообщаем всем, что клиент отключился
     broadcast(`${socket.name} left the chat.`, socket);
 }
 
+// Обрабатывает данные от клиентов
 function handleData(socket, data) {
 
     if (!sockets.includes(socket)) {
         return;
     }
 
+    // Преобразуем полученные данные в объект
     try {
         data = JSON.parse(data);
     }
     catch (err) {
         console.error(err);
+        data = {};
     };
 
+    // Считаем, что первое сообщение от клиента содержит его имя
     if (!socket.info) {
+
+        // Добавляем информацию о клиенте сокету
         addSocketInfo(socket, data);
-        socket.write(`Welcome ${socket.name}\n`);
+
+        // Отправляем приветствие клиенту
+        socket.write(`Welcome ${socket.name}.\n`);
         commands.info(socket);
         commands.help(socket);
-        broadcast(`${socket.name} joined the chat`, socket);
+
+        // Сообщаем остальным клиентам, что новый клиент подсоединился
+        broadcast(`${socket.name} joined the chat.`, socket);
     }
     else if(typeof data.text == 'string') {
         let text = data.text;
 
+        // Обрабатываем серверные команды и обычные сообщения
         if(text[0] == '/') {
             handleCommand(socket, text);
         }
-        else{
-            updateSocketInfo(socket);
+        else {
+            // Обновляем статистику сокета
+            updateSocketStats(socket);
+      
+            // Рассылаем сообщение остальным клиентам
             broadcast(`${socket.name}> ${text}`, socket);
         }
     }
@@ -110,42 +123,56 @@ function handleData(socket, data) {
     }
 }
 
+// Добавляет информацию сокету
 function addSocketInfo(socket, data) {
     let name = data.name || '';
     let lowerCaseName = name.toLowerCase();
 
+    // Даем пользователю стандартное имя, если имя не предоставлено или уже используется
     if (!name || names[lowerCaseName]) {
         socket.write('Name not provided, invalid or already taken!\n');
         name = `User${userIndex++}`;
         lowerCaseName = name.toLowerCase();
     }
 
+    // Добавляем имя сокету и добавляем имя в используемые
     socket.name = name;
     names[lowerCaseName] = socket;
 
+    // Краткая информация о сокете
     socket.info = socket.remoteAddress + ":" + socket.remotePort;
 }
 
-function updateSocketInfo(socket) {
+// Обновляет статистику сокета
+function updateSocketStats(socket) {
+
+    // Переносим сокет вперед массива
     sockets.splice(sockets.indexOf(socket), 1);
     sockets.unshift(socket);
+
+    // Последнее сообщение от клиента
     socket.lastActivity = Date.now();
+
+    // Кол-во сообщений от клиента
     socket.messagesNum++;
 }
 
+// Обрабатывает и исполняет серверную команду
 function handleCommand(socket, str) {
     let command = str.substr(1).match(/\w*/);
 
+    // Проверяем, существует ли команда
     if (command && commands[command]) {
         commands[command](socket);
-        return;
     }
     else {
-        socket.write(`Invalid command "${command}"\n`);
+        socket.write(`Invalid command "${command}".\n`);
     }
 }
 
+// Рассылает сообщение всем клиентам, кроме sender
 function broadcast(message, sender) {
+
     sockets.forEach(socket => {
 
         if (socket === sender){
@@ -158,7 +185,7 @@ function broadcast(message, sender) {
     console.log(`${sender.info}: ${message}`);
 }
 
-
+// Возвращает время в формате "32h 15m 55s"
 function formatTime(time) {
     let hours = Math.floor(time / 1000 / 60 / 60);
     let minutes = Math.floor((time - hours * 1000 * 60 * 60) / 1000 / 60);
@@ -174,4 +201,48 @@ function formatTime(time) {
     }
 
     return formatedTime;
+}
+
+
+/* Серверные команды */
+
+// Отправляет клиенту информацию и статистику всех клиентов
+function commandInfo(socket) {
+    let info = 'Currently in chat:\n';
+    sockets.forEach(otherSocket => {
+
+        // Время с последнего сообщения
+        let sinceActiveFormatted = formatTime(Date.now() - otherSocket.lastActivity);
+
+        // Имя клиента
+        if (otherSocket.name) {
+            info += `  ${otherSocket.name} (${otherSocket.info}) `;
+        }
+        else {
+            info += `  ${otherSocket.info} `;
+        }
+
+        // Кол-во сообщения
+        info += `has sent ${otherSocket.messagesNum} messages`;
+
+        // Время с последнего сообщения
+        if (otherSocket.messagesNum > 0) {
+            info += ` (last ${sinceActiveFormatted} ago)`;
+        }
+
+        info += '.\n';
+    });
+
+    socket.write(info);
+}
+
+// Отправляет клиенту список серверных команд
+function commandHelp(socket) {
+    let help = 'Commands:\n';
+
+    Object.keys(commands).forEach(command => {
+        help += `  /${command}\n`;
+    });
+
+    socket.write(help);
 }
